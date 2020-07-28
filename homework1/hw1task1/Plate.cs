d4a629fe66d88d512470660f317b913a6511e4ed
Ilya Roshupkin <roshupkin.ilya2012@yandex.ru>
2020-07-28 20:32:24 +0300

    it is ready

---
+++ homework1/hw1task1/Plate.cs
@@ -0,0 +1,29 @@
+﻿using MyGame;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGame
+{
+    class Plate: BaseObject 
+    {
+        public Plate(Point pos, Point dir, Size size) : base(pos, dir, size)
+        {
+        }
+        public override void Draw()
+        {
+            Game.Buffer.Graphics.DrawEllipse(Pens.White, Pos.X, Pos.Y, Size.Width, Size.Height);
+            Game.Buffer.Graphics.DrawEllipse(Pens.DarkGray, Pos.X+Size.Width/3, Pos.Y+Size.Height/3, Size.Width/3, Size.Height/3);
+        }
+        public override void Update()
+        {
+            Pos.X += Dir.X;
+            Pos.Y += Dir.Y;
+            Dir.X = (Pos.X < 0 || Pos.X >= Game.Width) ? -Dir.X : Dir.X;
+            Dir.Y = (Pos.Y < 0 || Pos.Y >= Game.Height) ? -Dir.Y : Dir.Y;
+        }
+    }
+}
---
+++ homework1/hw1task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+// Создаем шаблон приложения, где подключаем модули
+//1. Добавить свои объекты в иерархию объектов, чтобы получился красивый задний фон,
+//похожий на полет в звездном пространстве.
+//2. * Заменить кружочки картинками, используя метод DrawImage.
+//    Рощупкин
+namespace MyGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SplashScreen ss = new SplashScreen();
+            ss.ShowDialog();
+        }
+    }
+}
