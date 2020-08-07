ac156c1e3a18103beb48301fdc77a9ff6da70610
Ilya Roshupkin <roshupkin.ilya2012@yandex.ru>
2020-08-07 22:53:20 +0300

    homewok4

---
+++ homework4/MyGame/BaseObject.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MyGame
+{
+    abstract class BaseObject : ICollision
+    {
+        protected Point Pos;
+        protected Point Dir;
+        protected Size Size;
+        public delegate void Message();
+        public BaseObject(Point pos, Point dir, Size size)
+        {
+            
+                if (size.Width < 1) throw new GameObjectException("Ширина объекта слишком маленькая.");
+                if (size.Height < 1) throw new GameObjectException("Высота объекта слишком маленькая.");
+                if (Math.Abs(dir.X) > 30) throw new GameObjectException("Максимальная скорость объекта превышена.");
+                if (pos.X < 0 || pos.X > Game.Width) throw new GameObjectException("Неверная позиция объекта по абциссе.");
+                if (pos.Y < 0 || pos.Y > Game.Height) throw new GameObjectException("Неверная позиция объекта по ординате.");
+                Pos = pos;
+                Dir = dir;
+                Size = size;            
+        }
+        public static void AddMessage(string message)
+        {
+            Console.WriteLine(message);
+            Game.messagesJournal.Add(message);
+        }
+        public abstract void Draw();
+        public abstract void Update();
+        public bool Collision(ICollision o) => o.Rect.IntersectsWith(Rect);
+        public Rectangle Rect => new Rectangle(Pos, Size);
+    }
+}
---
+++ homework4/MyGame/SplashScreen.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace MyGame
+{
+    partial class SplashScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SplashScreen));
+            this.btnStartGame = new System.Windows.Forms.Button();
+            this.btnRecords = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblDev = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStartGame
+            // 
+            this.btnStartGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnStartGame.Location = new System.Drawing.Point(127, 79);
+            this.btnStartGame.Name = "btnStartGame";
+            this.btnStartGame.Size = new System.Drawing.Size(242, 40);
+            this.btnStartGame.TabIndex = 0;
+            this.btnStartGame.Text = "Начать Игру";
+            this.btnStartGame.UseVisualStyleBackColor = true;
+            this.btnStartGame.Click += new System.EventHandler(this.btnStartGame_Click);
+            // 
+            // btnRecords
+            // 
+            this.btnRecords.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnRecords.Location = new System.Drawing.Point(127, 148);
+            this.btnRecords.Name = "btnRecords";
+            this.btnRecords.Size = new System.Drawing.Size(242, 40);
+            this.btnRecords.TabIndex = 1;
+            this.btnRecords.Text = "Рекорды";
+            this.btnRecords.UseVisualStyleBackColor = true;
+            this.btnRecords.Click += new System.EventHandler(this.btnRecords_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnExit.Location = new System.Drawing.Point(127, 217);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(242, 40);
+            this.btnExit.TabIndex = 2;
+            this.btnExit.Text = "Выход";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblDev
+            // 
+            this.lblDev.AutoSize = true;
+            this.lblDev.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblDev.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblDev.Location = new System.Drawing.Point(177, 296);
+            this.lblDev.Name = "lblDev";
+            this.lblDev.Size = new System.Drawing.Size(147, 17);
+            this.lblDev.TabIndex = 3;
+            this.lblDev.Text = "Рощупкин Илья 2020";
+            // 
+            // SplashScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(488, 363);
+            this.Controls.Add(this.lblDev);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnRecords);
+            this.Controls.Add(this.btnStartGame);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "SplashScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Dangerous Space";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartGame;
+        private System.Windows.Forms.Button btnRecords;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblDev;
+    }
+}
