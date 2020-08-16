06ea356811115fd65715073bd5231711f95d1bcd
Ilya Roshupkin <roshupkin.ilya2012@yandex.ru>
2020-08-16 16:48:43 +0300

    homework6

---
+++ homework6/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace redidHW
+{
+    class Employee : INotifyPropertyChanged
+    {
+        private string fullName;
+        public string FullName
+        {
+            get { return fullName; }
+            set
+            {
+                if (fullName != value)
+                {
+                    fullName = value;
+                    NotifyPropertyChanged("FullName");
+                }
+            }
+        }
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+        private Department department;
+        public Department Department
+        {
+            get { return department; }
+            set
+            {
+                if (department != value)
+                {
+                    department = value;
+                    NotifyPropertyChanged("Department");
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
